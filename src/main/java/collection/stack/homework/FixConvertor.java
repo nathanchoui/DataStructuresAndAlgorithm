fc70dc39f87ff969e81330862695b402c016cad1
zhangwei <zhangwei@lieying.cn>
2018-03-26 14:06:09 +0800

    add

---
+++ src/main/java/collection/stack/homework/FixConvertor.java
@@ -0,0 +1,45 @@
+package collection.stack.homework;
+
+/**
+ * Description:
+ * <p>
+ * Created by zhangwei on 2018/3/26.
+ */
+public class FixConvertor {
+
+    private static final String[] OPERAND = {"+", "-", "*", "/"};
+
+
+    /**
+     * 判断是否是合法的中缀表达式
+     *
+     * @param expression
+     * @return
+     */
+    public boolean isLeagleExpression(String[] expression) {
+        return false;
+    }
+
+    /**
+     * 中缀表达式转换成后缀表达式
+     * @param nifixExp
+     * @return
+     */
+    public String[] nifix2Postfix(String[] nifixExp) {
+
+
+        return null;
+    }
+
+    /**
+     * 计算后缀表达式的值
+     *
+     * @param postfixExp
+     * @return
+     */
+    public Number caculatePostfix(String[] postfixExp) {
+        return  null;
+    }
+
+
+}
